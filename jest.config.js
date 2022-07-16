2430ea851f85ea391b2e2f058f740bae5af6381e
Rafael-Messias <rfamepi@hotmail.com>
2022-07-16 15:22:44 -0300

    refactor: configs eslint and jest.config

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   collectCoverage: true,
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**'
+  ],
   coverageDirectory: 'coverage',
   roots: [
     '<rootDir>/src'
