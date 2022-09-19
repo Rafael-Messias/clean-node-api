8ecc1a350723dbe609823509894eb1df9ac6feaa
rafael <rafael@acaciaconsultoria.com.br>
2022-09-19 16:05:47 -0300

    refactor: move Validation to protocols folder

--- src/presentation/helpers/validator/validation-composite.spec.ts
+++ src/presentation/helpers/validator/validation-composite.spec.ts
@@ -1,6 +1,6 @@
 
 import { MissingParamError } from '../../errors'
-import { Validation } from './validation'
+import { Validation } from '../../protocols/validation'
 import { ValidationComposite } from './validation-composite'
 
 interface SutTypes {
