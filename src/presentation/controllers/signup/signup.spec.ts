16faf01f1e926fee8e6c3c5fb3f0e04e2eb5bca0
rafael <rafael@acaciaconsultoria.com.br>
2022-09-19 15:47:37 -0300

    refactor: move HttpHelper to a folder

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -1,7 +1,7 @@
 import { SignUpController } from './signup'
 import { AccountModel, AddAccount, AddAccountModel, HttpRequest, Validation } from '../signup/signup-protocols'
 import { MissingParamError, ServerError } from '../../errors'
-import { badRequest, ok, serverError } from '../../helpers/http-helper'
+import { badRequest, ok, serverError } from '../../helpers/http/http-helper'
 
 const makeAddAccount = (): AddAccount => {
   class AddAccountStub implements AddAccount { // Stub - um dublê de testes
